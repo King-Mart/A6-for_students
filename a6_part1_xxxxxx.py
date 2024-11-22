f88645bd5687ba7f66cb8496025187f6a2e5bd06
KMart <ken.mart.d@gmail.com>
2024-11-22 17:44:54 -0500

    finished part 1

--- a6_part1_xxxxxx.py
+++ a6_part1_xxxxxx.py
@@ -1,8 +1,14 @@
 import string
 
-def open_file() :
+
+minimalLenght : int = 2
+def open_file() -> object:
     '''None->file object
-    See the assignment text for what this function should do'''
+    Prompts the user to type the name of a file to open.
+    If the file does not exist, it keeps asking until a valid file name is entered.
+    The file name has to have a .txt extension.
+    The file is opened in utf-8 encoding. INstead of the device specific encoding
+    '''
     # YOUR CODE GOES HERE
     validFileName : bool = False
     while not validFileName:
@@ -15,50 +21,73 @@ def open_file() :
 #Tells you if a character is not a letter
 def notalpha(char : str) -> bool:return not str.isalpha(char)
 
-def lineParser(line : str) -> list[str]:
-    resultLine : list[str] = []
-    currentword = ''
-    index = 0
-    lenght = 0
+def lineParser(line : str, minimalLenght : int = minimalLenght) -> list[str]:
+    '''(str)->list of str
+    Takes a string, and breaks it up into a list of words. 
+    A word is defined as a sequence of alphabetic characters.longer than a minimal value
+    Non-alphabetic characters are ignored. This is case insensitive.
+    args:
+        line : A string of words to be broken up. 
+    return:
+        A list of the words., empty if the parameter is an empty string
+    '''
+    resultLine : list[str] = []; currentword = ''; index = 0
     while index <  len(line):
         if line[index] == " ":
-            if lenght >= 2 : resultLine.append(currentword); 
-            currentword = ""; lenght = 0
+            if len(currentword) >= minimalLenght : resultLine.append(currentword); 
+            currentword = ""
 
         elif line[index] in string.punctuation: pass
-        elif line[index] in "'-.": pass
-        elif not str.isalpha(line[index]):
-            print(f"THis cuased me a hiccup : {line[index]}")
+        elif line[index] in "'-.\n": pass
+        elif not line[index].isalpha():
             currentword = ''
             while index < len(line) :
-                if line[index] != " ": break
+                if line[index] == " ": break
                 index += 1
-        else: currentword += line[index]; lenght += 1
+
+        else: currentword += line[index].lower(); 
         index +=1
-    print(f"curr word :{currentword}")
-    if lenght >= 2 : resultLine.append(currentword)
+    if len(currentword) >= minimalLenght : resultLine.append(currentword)
     return resultLine
     
 def read_file(fp : object):
     '''(file object)->dict
-    See the assignment text for what this function should do'''
+    Given a file pointer, read the file and return a dictionary of words to the lines they appear on.
+    The keys in the dictionary are the words, and the values are sets of line numbers.
+    The line numbers are 1-indexed; i.e. the first line is line 1.
+    If a word appears on multiple lines, the set will contain all of those line numbers.
+    If a word does not appear in the file, it will not be in the dictionary.
+    The dictionary is case insensitive.
+    '''
     # YOUR CODE GOES HERE
     occurenceMap = {}
     for index, line in enumerate(fp):
-        for word in lineParser(line):
-            print(word)
-            resultSet = occurenceMap.setdefault(word, set())
-            resultSet.add(index + 1)
-            # print(resultSet)
-        print(line)
-        print(lineParser(line))
+        for word in lineParser(line): resultSet = occurenceMap.setdefault(word, set()); resultSet.add(index + 1); occurenceMap[word] = resultSet
+    return occurenceMap
 
-def find_coexistance(D, query):
-    '''(dict[str, set[int],str)->list
-    See the assignment text for what this function should do'''
-    # YOUR CODE GOES HERE
-    pass
-    
+def find_coexistance(D : dict[str, set[int]], query : str) -> list[int | str]:
+    '''(dict[str, set[int],str)->list[int | str]
+    Given a dictionary D of words in a file to the lines they appear on,
+    and a query string, return a list of the line numbers of all the lines where ALL the words
+    in the query appear. If a word in the query does not appear in the file,
+    return a message indicating that the word was not found.
+    If no line has all the words, return a message indicating that.
+    '''
+    # Parse the query string into a list of words
+    parsedQuery = lineParser(query)
+    if parsedQuery == []: parsedQuery = ['']
+    intersect : set = D.get(parsedQuery[0], set())
+    if intersect == set() : return [f"Word '{parsedQuery[0]}' not in file"]
+    pointer = 1
+    # loop through the rest of the words in the query
+    while intersect and pointer < len(parsedQuery): # if the current word is in the dictionary, intersect with its line numbers
+        if parsedQuery[pointer] in D : intersect = intersect.intersection(D[parsedQuery[pointer]]) # if the current word is not in the dictionary, return an appropriate message
+        else : return [f"Word '{parsedQuery[pointer]}' not in file"] # if we have exhausted all the words in the query, and there is no intersection, return an appropriate message
+        if intersect == set() : return [f"The one or more words you entered does not coexist in a same line of the file.\nCause : '{parsedQuery[pointer]}' not found on any of the lines where previous word where"]
+        pointer += 1 
+    return ["The one or more words you entered does not coexist in a same line of the file.\n"] + sorted(list(intersect)) # if we have exhausted all the words in the query, and there is an intersection, return the sorted list of line numbers
+
+
 
 ##############################
 # main
@@ -68,4 +97,9 @@ d=read_file(file)
 query=input("Enter one or more words separated by spaces, or 'q' to quit: ").strip().lower()
 
 # YOUR CODE GOES HERE
+
+while query != 'q':
+    for response in find_coexistance(d, query):
+        print(response, end= " ")
+    query=input("\nEnter one or more words separated by spaces, or 'q' to quit: ").strip().lower()
 
